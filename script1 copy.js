dfaab30305ff50682bb36ceb28a57e60b97ac68b
natikkr <82267830+natikkr@users.noreply.github.com>
2021-08-26 15:20:48 +0300

    Add files via upload

--- script1 copy.js
+++ script1 copy.js
@@ -1,84 +1,47 @@
 class Clock {
-    constructor(element){
+    constructor(element) {
         this.element = element;
-        }
-        Run() {
-            this.render();
-
-        setInterval(() =>{
-            this.render();
-            }, 1000);
-        }
-
-        render() {
-           let time = this.getTime();
-           let minForm = time.minutes.toString().padStart(2,"0");
-           let secForm = time.seconds.toString().padStart(2, "0")
-           let timeForm = `${time.hour}:${minForm}`;
-           let timeFull = `${time.hour}:${minForm}:${secForm}`;
-           console.log(timeFull)
-           
-           if (clockDisplayElement.classList.contains('hidden')){
-            this.element.innerHTML = timeForm;
-            } else {
-                this.element.innerHTML = timeFull;  
-            }
-        }
-
-        getTime(){
-            let now = new Date();
-            return{
-                hour: now.getHours(),
-                minutes: now.getMinutes(),
-                seconds: now.getSeconds()
-            };
-        }
-        
     }
-    class ClockForm extends Clock{
-        constructor(element, timeForm){
-            super(element, timeForm)
-            this.element = element   
-        }
-        render(timeForm) {
-            this.element.innerHTML = timeForm
-            console.log(timeForm)         
-        }      
+    render() {
+        let time = new Date;
+        let hours = time.getHours();
+        this.element.innerHTML = `${hours}`
+    };
+
+};
+
+class ClockFull extends Clock {
+    render() {
+        let time = new Date;
+        let hours = time.getHours();
+        let minutes = time.getMinutes();
+        let seconds = time.getSeconds()
+        this.element.innerHTML = `${hours}:${minutes}:${seconds}`
+    };
+};
+
+class ClockForm extends Clock {
+    render() {
+        let time = new Date;
+        let hours = time.getHours();
+        let minutes = time.getMinutes();
+        this.element.innerHTML = `${hours}:${minutes}`;
+    };
+};
+
+let clockDisplayElement = document.querySelector(".clockpage")
+let timeFull = new ClockFull (clockDisplayElement);
+let timeForm = new ClockForm (clockDisplayElement);
+
+clockDisplayElement.addEventListener('click', (event) =>{
+event.target.classList.toggle('hidden')
+  })
+
+setInterval(() => {
+    if (clockDisplayElement.classList.contains('hidden')) {
+        timeForm.render();
+    } else {
+        timeFull.render();
     }
-    
-    class ClockFull extends Clock{
-        constructor(element, timeFull){
-            super(element, timeFull)
-            this.element = element             
-        }
-      
-        render(timeFull) {
-            this.element.innerHTML = timeFull
-            console.log(timeFull)   
-        }
-        Run() {
-            this.render();
-
-        setInterval(() =>{
-            this.render();
-            }, 1000);
-        }
-}
-  
-    let clockDisplayElement = document.querySelector(".clockpage");
-    let clockObject = new ClockFull(clockDisplayElement);
-  
-    clockObject.Run();
-
-    document.querySelector(".clockpage").addEventListener('click', (event) =>{
-        event.target.classList.toggle('hidden');
-    })
-    let clockObject2 = new ClockForm(clockDisplayElement);
-  
-    clockObject2.Run();
-
-    document.querySelector(".clockpage").addEventListener('click', (event) =>{
-        event.target.classList.toggle('hidden');
-    })
-
+}, 100)
 
